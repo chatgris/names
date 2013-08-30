e7749f815da9141dbe99728bd13add3bd7993983
chatgris <jboyer@af83.com>
2013-08-30 17:46:53 +0200

    Remove generated example
    Signed-off-by: chatgris <jboyer@af83.com>

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -8,15 +8,6 @@ defmodule ApplicationRouter do
     conn.fetch([:cookies, :params])
   end
 
-  # It is common to break your Dynamo in many
-  # routers forwarding the requests between them
-  # forward "/posts", to: PostsRouter
-
-  get "/" do
-    conn = conn.assign(:title, "Welcome to Dynamo!")
-    render conn, "index.html"
-  end
-
   get "/hello/:name" do
     conn = conn.put_resp_header "Content-Type", "application/json"
     conn.resp 200, JSON.generate [Hello: conn.params[:name]]
